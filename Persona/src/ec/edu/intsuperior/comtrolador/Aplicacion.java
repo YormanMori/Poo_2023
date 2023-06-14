31dec02c8c990c64ef185e6c272279d7d3badf46
YormanMori <User@YORMAN>
2023-06-14 01:42:13 -0500

    esta version es la 1.2

---
+++ Persona/src/ec/edu/intsuperior/comtrolador/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.controlador.Controlador;
+
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        Controlador control= new Controlador();
+        control.menu();
+    }
+    
+}
